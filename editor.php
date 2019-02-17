746c22921938f6f7d27f6dce6a724f854d1dbd25
boxseto <35777054+boxseto@users.noreply.github.com>
2019-02-17 17:18:53 +0800

    Update editor.php

--- editor.php
+++ editor.php
@@ -184,7 +184,7 @@ if(isset($_REQUEST['config'])){
             ));
         */    
         try{
-          $upload = $s3->upload($bucket, $file_name, fopen($tmp, 'rb'), 'public-read');
+          $upload = $s3->upload($bucket, $_COOKIE['filename'], fopen($tmp, 'rb'), 'public-read');
           echo htmlspecialchars($upload->get('ObjectURL'));
         }catch(Exception $e){echo $e->getMessage();}
         echo 's3 create object finished';
